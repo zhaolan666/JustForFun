062f2dd7721537c475a06115de997132df6acc1f
mingdu <wy1412185335@163.com>
2023-02-23 10:59:47 +0800

    chore: testing ..

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vitest/config'
 import Vue from '@vitejs/plugin-vue'
 import VueJsx from '@vitejs/plugin-vue-jsx'
-import VueMacros from 'unplugin-vue-macros/vite'
+import vueMacros from 'unplugin-vue-macros/vite'
 
 export default defineConfig({
   plugins: [
-    VueMacros({
+    vueMacros({
       setupComponent: false,
       setupSFC: false,
       plugins: {
