f28ec8c666d05422608e333f32c6ed2e98bc3005
mingdu <wy1412185335@163.com>
2023-03-12 22:38:23 +0800

    docs: change theme  and add content

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,271 @@
+import {
+  createRequire
+} from 'module'
+import {
+  defineConfig
+} from 'vitepress'
+
+const require = createRequire(
+  import.meta.url)
+const pkg = require('vitepress/package.json')
+
+export default defineConfig({
+  lang: 'en-US',
+  title: 'JustForFun',
+  base: 'https://github.com/zhaolan666/JustForFun/tree/dev',
+  description: 'JustForFun is a simple UI framework',
+
+  lastUpdated: true,
+  cleanUrls: true,
+
+  head: [
+    ['meta', { name: 'theme-color', content: '#845EC2' }],
+    ['link', {
+      rel: 'icon',
+      href: '/docs/public/images/jffLogo.png'
+    }]
+  ],
+
+  markdown: {
+    headers: {
+      level: [0, 0]
+    }
+  },
+
+  themeConfig: {
+    nav: nav(),
+
+    sidebar: {
+      '/guide/': sidebarGuide(),
+      '/config/': sidebarConfig()
+    },
+
+    editLink: {
+      pattern: 'https://github.com/zhaolan666/JustForFun/issues',
+      text: 'Edit this page on GitHub'
+    },
+
+    socialLinks: [{
+      icon: 'github',
+      link: 'https://github.com/zhaolan666/JustForFun/tree/dev'
+    }],
+
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2023-present  mingdu'
+    },
+    algolia: {
+      appId: '8J64VVRP8K',
+      apiKey: 'a18e2f4cc5665f6602c5631fd868adfd',
+      indexName: 'vitepress'
+    },
+
+    carbonAds: {
+      code: 'CEBDT27Y',
+      placement: 'vuejsorg'
+    }
+  }
+})
+
+function nav() {
+  return [{
+    text: 'Guide',
+    link: '/guide/example',
+    activeMatch: '/guide/'
+  },
+  {
+    text: 'Configs',
+    link: '/config/cofig',
+    activeMatch: '/config/'
+  },
+  {
+    text: pkg.version,
+    items: [{
+      text: '中文文档',
+      link: 'https://github.com/zhaolan666/JustForFun/blob/dev/README.zh-CN.md'
+    },
+    {
+      text: '英文文档',
+      link: 'https://github.com/zhaolan666/JustForFun/blob/dev/README.md'
+    }
+    ]
+  }
+  ]
+}
+
+function sidebarGuide() {
+  return [{
+    text: 'Basic',
+    collapsed: false,
+    items: [{
+      text: 'Button',
+      link: '/guide/example',
+    },
+    {
+      text: 'Border',
+      link: '/guide/getting-started'
+    },
+    {
+      text: 'Color',
+      link: '/guide/configuration'
+    },
+    {
+      text: 'Icon',
+      link: '/guide/deploying'
+    },
+    {
+      text: 'Layout',
+      link: '/guide/i18n'
+    }
+    ]
+  },
+  {
+    text: 'Configuration',
+    collapsed: false,
+    items: [{
+      text: 'Config Provider',
+      link: '/guide/markdown'
+    }]
+  },
+  {
+    text: 'From',
+    collapsed: false,
+    items: [{
+      text: 'Checkbox',
+      link: '/guide/theme-introduction'
+    },
+    {
+      text: 'Color Picker',
+      link: '/guide/theme-nav'
+    },
+    {
+      text: 'Date Picker',
+      link: '/guide/theme-sidebar'
+    },
+    {
+      text: 'Date Time Picker',
+      link: '/guide/theme-prev-next-link'
+    },
+    {
+      text: 'From',
+      link: '/guide/theme-edit-link'
+    },
+    {
+      text: 'Input',
+      link: '/guide/theme-last-updated'
+    },
+    {
+      text: 'Rido',
+      link: '/guide/theme-layout'
+    },
+    {
+      text: 'Select',
+      link: '/guide/theme-home-page'
+    },
+    {
+      text: 'Switch',
+      link: '/guide/theme-team-page'
+    },
+    {
+      text: 'Upload',
+      link: '/guide/theme-badge'
+    },
+    ]
+  },
+  {
+    text: 'Data',
+    collapsed: false,
+    items: [{
+      text: 'Avatar',
+      link: '/guide/migration-from-vuepress'
+    },
+    {
+      text: 'Card',
+      link: '/guide/migration-from-vuepress'
+    },
+    {
+      text: 'Image',
+      link: '/guide/migration-from-vitepress-0'
+    },
+    {
+      text: 'Tag',
+      link: '/guide/migration-from-vitepress-0'
+    }
+    ]
+  },
+  {
+    text: 'Navigation',
+    collapsed: false,
+    items: [{
+      text: 'Dropdown',
+      link: '/guide/migration-from-vuepress'
+    },
+    {
+      text: 'Menu',
+      link: '/guide/migration-from-vuepress'
+    },
+    {
+      text: 'Steps',
+      link: '/guide/migration-from-vitepress-0'
+    },
+    {
+      text: 'Tabs',
+      link: '/guide/migration-from-vitepress-0'
+    }
+    ]
+  },
+  {
+    text: 'Feedback',
+    collapsed: false,
+    items: [{
+      text: 'Alert',
+      link: '/guide/migration-from-vuepress'
+    },
+    {
+      text: 'Dialog',
+      link: '/guide/migration-from-vuepress'
+    },
+    {
+      text: 'Loading',
+      link: '/guide/migration-from-vitepress-0'
+    },
+    {
+      text: 'Message',
+      link: '/guide/migration-from-vitepress-0'
+    }
+    ]
+  },
+  {
+    text: 'Others',
+    collapsed: false,
+    items: [{
+      text: 'Diver',
+      link: '/guide/migration-from-vuepress'
+    }]
+  }
+  ]
+}
+
+
+function sidebarConfig() {
+  return [{
+    text: 'Config',
+    items: [{
+      text: 'Introduction',
+      link: '/config/introduction'
+    },
+    {
+      text: 'App Configs',
+      link: '/config/app-configs'
+    },
+    {
+      text: 'Theme Configs',
+      link: '/config/theme-configs'
+    },
+    {
+      text: 'Frontmatter Configs',
+      link: '/config/frontmatter-configs'
+    }
+    ]
+  }]
+}
--- docs/index.md
+++ docs/index.md
@@ -1,30 +1,58 @@
 ---
 layout: home
 
+title: Justforfun-UI
+titleTemplate: UI component library
+
 hero:
   name: JustForFun-UI
   text: UI component library
   tagline: JustForFun is a simple, quick and easy to use Vue3 combined UI framework.
   image:
-      src: /public/jffTitle.png
+      src: /docs/public/jffTitle.png
       alt: justforfun-ui
       
   actions: 
     - theme: brand
       text: Get Started
-      link: /guide/example
+      link: /docs/guide/example
     - theme: alt
       text: View on GitHub
       link: https://github.com/zhaolan666/JustForFun/tree/dev
 
 features:
-    - title: Vue3 based
+    - icon: 
+        src: /docs/public/vue.svg
+        width: 10
+        height: 10
+      title: Vue3 based
       details: Popular easy to use and expansibility strong SFC writing
-    - title: TypeScript based
+    - icon: 
+        src: /docs/public/typescript.svg
+        width: 10
+        height: 10
+      title: TypeScript based
       details: Source code uses Typescript type constraints
-    - title: Code simplicity
+    - icon: 
+        src: /docs/public/tidy.svg
+        width: 10
+        height: 10
+      title: Code simplicity
       details: The source code for each component is extremely clean and concise
 
 ---      
 
 <iframe src="https://stackblitz.com/edit/vue-lbh1hk?file=src%2Fmain.js" style="width:100%; margin:50px auto; padding: 10px; height: 700px; border: 0; border-radius: 4px; overflow: hidden;" title="justforfun-ui"></iframe>
+
+
+
+<style>
+  :root {
+     --vp-home-hero-name-color: transparent;
+  --vp-home-hero-name-background: -webkit-linear-gradient(120deg, #BE93FD, #4FFBDF);
+  --vp-c-brand: #845EC2;
+  --vp-button-brand-border: #BE93FD;
+  --vp-button-brand-hover-border: #DCB0FF;
+  --vp-button-brand-hover-bg: #593796;
+  }
+</style>
